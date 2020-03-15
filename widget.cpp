e228373acebce077e8437d901acd03115f0e8881
mincheng.luo <mincheng.luo@ubtrobot.com>
2020-03-15 21:40:06 +0800

    添加ncnn库

--- widget.cpp
+++ widget.cpp
@@ -6,10 +6,131 @@
 
 using namespace std;
 
+#ifndef MIN
+#  define MIN(a,b)  ((a) > (b) ? (b) : (a))
+#endif
+
+#ifndef MAX
+#  define MAX(a,b)  ((a) < (b) ? (b) : (a))
+#endif
+
+float InterRectArea(const QRect & a, const QRect & b) {
+    QPoint left_top = QPoint(MAX(a.x(), b.x()), MAX(a.y(), b.y()));
+    QPoint right_bottom = QPoint(MIN(a.right(), b.right()), MIN(a.bottom(), b.bottom()));
+    QPoint diff = right_bottom - left_top;
+    return (MAX(diff.x() + 1, 0) * MAX(diff.y() + 1, 0));
+}
+
+int ComputeIOU(const QRect & rect1,
+    const QRect & rect2, float * iou,
+    const std::string& type) {
+
+    float inter_area = InterRectArea(rect1, rect2);
+    if (type == "UNION") {
+        *iou = inter_area / (rect1.width()*rect1.height() + rect2.width()*rect2.height() - inter_area);
+    }
+    else {
+        *iou = inter_area / MIN(rect1.width()*rect1.height(), rect2.width()*rect2.height());
+    }
+
+    return 0;
+}
+
+template <typename T>
+int const NMS(const std::vector<T>& inputs, std::vector<T>* result,
+    const float& threshold, const std::string& type = "UNION") {
+    result->clear();
+    if (inputs.size() == 0)
+        return -1;
+
+    std::vector<T> inputs_tmp;
+    inputs_tmp.assign(inputs.begin(), inputs.end());
+    std::sort(inputs_tmp.begin(), inputs_tmp.end(),
+    [](const T& a, const T& b) {
+        return a.score_ < b.score_;
+    });
+
+    std::vector<int> indexes(inputs_tmp.size());
+
+    for (int i = 0; i < indexes.size(); i++) {
+        indexes[i] = i;
+    }
+
+    while (indexes.size() > 0) {
+        int good_idx = indexes[0];
+        result->push_back(inputs_tmp[good_idx]);
+        std::vector<int> tmp_indexes = indexes;
+        indexes.clear();
+        for (int i = 1; i < tmp_indexes.size(); i++) {
+            int tmp_i = tmp_indexes[i];
+            float iou = 0.0f;
+            ComputeIOU(inputs_tmp[good_idx].location_, inputs_tmp[tmp_i].location_, &iou, type);
+            if (iou <= threshold) {
+                indexes.push_back(tmp_i);
+            }
+        }
+    }
+}
+
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      img(640, 360, QImage::Format_RGB888)
 {
     setGeometry(200, 200, 640, 480);
+    pnet_ = new ncnn::Net();
+    rnet_ = new ncnn::Net();
+    onet_ = new ncnn::Net();
+
+
+    pnet_size_ = 12;
+    min_face_size_ = 40;
+    scale_factor_ = 0.709f;
+    initialized_ = false;
+
+
+
+
+
+
+
+    string root_path = "/home/lmc/Desktop/untitled/models";
+    std::string pnet_param = std::string(root_path) + "/pnet.param";
+    std::string pnet_bin = std::string(root_path) + "/pnet.bin";
+    if (pnet_->load_param(pnet_param.c_str()) == -1 ||
+        pnet_->load_model(pnet_bin.c_str()) == -1) {
+        std::cout << "Load pnet model failed." << std::endl;
+        std::cout << "pnet param: " << pnet_param << std::endl;
+        std::cout << "pnet bin: " << pnet_bin << std::endl;
+    }
+    std::string rnet_param = std::string(root_path) + "/rnet.param";
+    std::string rnet_bin = std::string(root_path) + "/rnet.bin";
+    if (rnet_->load_param(rnet_param.c_str()) == -1 ||
+        rnet_->load_model(rnet_bin.c_str()) == -1) {
+        std::cout << "Load rnet model failed." << std::endl;
+        std::cout << "rnet param: " << rnet_param << std::endl;
+        std::cout << "rnet bin: " << rnet_bin << std::endl;
+    }
+    std::string onet_param = std::string(root_path) + "/onet.param";
+    std::string onet_bin = std::string(root_path) + "/onet.bin";
+    if (onet_->load_param(onet_param.c_str()) == -1 ||
+        onet_->load_model(onet_bin.c_str()) == -1) {
+        std::cout << "Load onet model failed." << std::endl;
+        std::cout << "onet param: " << onet_param << std::endl;
+        std::cout << "onet bin: " << onet_bin << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     connect(this, SIGNAL(touchFrame()), this, SLOT(frameSlot()));
     av_register_all();
     QString filename = "/home/lmc/Desktop/test.mp4";
@@ -241,6 +362,69 @@ Widget::Widget(QWidget *parent)
 
 
 
+
+
+                    memcpy(this->img.bits(), this->pFrameYUV->data[0], 640*360*3);
+                    ncnn::Mat img_in = ncnn::Mat::from_pixels(this->pFrameYUV->data[0],
+                        ncnn::Mat::PIXEL_RGB, 640, 360);
+
+                    QSize max_size = {640, 360};
+
+                    std::vector<FaceInfo> faces;
+
+                    img_in.substract_mean_normalize(this->meanVals, this->normVals);
+                printf("11111111111111111\n");
+                    std::vector<FaceInfo> first_bboxes, second_bboxes;
+                    std::vector<FaceInfo> first_bboxes_result;
+                    this->PDetect(img_in, &first_bboxes);
+                    NMS(first_bboxes, &first_bboxes_result, this->nms_threshold_[0]);
+
+                    this->Refine(&first_bboxes_result, max_size);
+
+                    this->RDetect(img_in, first_bboxes_result, &second_bboxes);
+
+                    std::vector<FaceInfo> second_bboxes_result;
+                    NMS(second_bboxes, &second_bboxes_result, this->nms_threshold_[1]);
+
+                    this->Refine(&second_bboxes_result, max_size);
+
+                    std::vector<FaceInfo> third_bboxes;
+                    this->ODetect(img_in, second_bboxes_result, &third_bboxes);
+
+                    NMS(third_bboxes, &faces, this->nms_threshold_[2], "MIN");
+
+                    this->Refine(&faces, max_size);
+                    printf("222222222222222222\n");
+                    QPainter pp(&img);
+
+                    for (auto &tmp : faces)
+                    {
+                        pp.drawRect(tmp.location_);
+                    }
+
+
+                    memcpy(this->pFrameYUV->data[0], img.bits(), 640*360*3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     static int64_t mPTS = 0;
                     sws_scale(this->img_convert_rgb_ctx, this->pFrameYUV->data, this->pFrameYUV->linesize, 0, 360,
                                 this->mYUVFrm->data, this->mYUVFrm->linesize
@@ -279,7 +463,7 @@ Widget::Widget(QWidget *parent)
 
 
                     this->touchFrame();
-                    usleep(3000);
+                  //  usleep(3000);
 
                 }
             }
@@ -343,9 +527,199 @@ void Widget::frameSlot()
 void Widget::paintEvent(QPaintEvent *e)
 {
     QPainter p(this);
-    QImage pi(640, 360, QImage::Format_RGB888);
 
-    memcpy(pi.bits(), pFrameYUV->data[0], 640*360*3);
+
+
+
+
+
+
+
+
+
+
+
   //  memset(pi.bits(), a, 640*480*3);
-    p.drawImage(0, 0, pi);
+    p.drawImage(0, 0, img);
 }
+
+
+
+
+int Widget::PDetect(const ncnn::Mat & img_in,
+    std::vector<FaceInfo>* first_bboxes) {
+    first_bboxes->clear();
+    int width = img_in.w;
+    int height = img_in.h;
+    float min_side = MIN(width, height);
+    float curr_scale = float(pnet_size_) / min_face_size_;
+    min_side *= curr_scale;
+    std::vector<float> scales;
+    while (min_side > pnet_size_) {
+        scales.push_back(curr_scale);
+        min_side *= scale_factor_;
+        curr_scale *= scale_factor_;
+    }
+
+    // mutiscale resize the image
+    for (int i = 0; i < static_cast<size_t>(scales.size()); ++i) {
+        int w = static_cast<int>(width * scales[i]);
+        int h = static_cast<int>(height * scales[i]);
+        ncnn::Mat img_resized;
+        ncnn::resize_bilinear(img_in, img_resized, w, h);
+        ncnn::Extractor ex = pnet_->create_extractor();
+        //ex.set_num_threads(2);
+        ex.set_light_mode(true);
+        ex.input("data", img_resized);
+        ncnn::Mat score_mat, location_mat;
+        ex.extract("prob1", score_mat);
+        ex.extract("conv4-2", location_mat);
+        const int stride = 2;
+        const int cell_size = 12;
+        for (int h = 0; h < score_mat.h; ++h) {
+            for (int w = 0; w < score_mat.w; ++w) {
+                int index = h * score_mat.w + w;
+                // pnet output: 1x1x2  no-face && face
+                // face score: channel(1)
+                float score = score_mat.channel(1)[index];
+                if (score < threshold_[0]) continue;
+
+                // 1. generated bounding box
+                int x1 = round((stride * w + 1) / scales[i]);
+                int y1 = round((stride * h + 1) / scales[i]);
+                int x2 = round((stride * w + 1 + cell_size) / scales[i]);
+                int y2 = round((stride * h + 1 + cell_size) / scales[i]);
+
+                // 2. regression bounding box
+                float x1_reg = location_mat.channel(0)[index];
+                float y1_reg = location_mat.channel(1)[index];
+                float x2_reg = location_mat.channel(2)[index];
+                float y2_reg = location_mat.channel(3)[index];
+
+                int bbox_width = x2 - x1 + 1;
+                int bbox_height = y2 - y1 + 1;
+
+                FaceInfo face_info;
+                face_info.score_ = score;
+                face_info.location_.setLeft(x1 + x1_reg * bbox_width);
+                face_info.location_.setTop(y1 + y1_reg * bbox_height);
+                face_info.location_.setRight(x2 + x2_reg * bbox_width);
+                face_info.location_.setBottom(y2 + y2_reg * bbox_height);
+\
+          //      face_info.location_ = face_info.location_ & cv::Rect(0, 0, width, height);
+                first_bboxes->push_back(face_info);
+            }
+        }
+    }
+    return 0;
+}
+
+int Widget::RDetect(const ncnn::Mat & img_in,
+    const std::vector<FaceInfo>& first_bboxes,
+    std::vector<FaceInfo>* second_bboxes) {
+
+    second_bboxes->clear();
+    for (int i = 0; i < static_cast<int>(first_bboxes.size()); ++i) {
+
+        QRect face = first_bboxes.at(i).location_ & QRect(0, 0, img_in.w, img_in.h);
+        ncnn::Mat img_face, img_resized;
+        ncnn::copy_cut_border(img_in, img_face, face.y(), img_in.h - face.bottom(), face.x(), img_in.w - face.right());
+
+        ncnn::resize_bilinear(img_face, img_resized, 24, 24);
+
+        ncnn::Extractor ex = rnet_->create_extractor();
+        ex.set_light_mode(true);
+        ex.set_num_threads(2);
+        ex.input("data", img_resized);
+        ncnn::Mat score_mat, location_mat;
+        ex.extract("prob1", score_mat);
+        ex.extract("conv5-2", location_mat);
+        float score = score_mat[1];
+        if (score < threshold_[1]) continue;
+        float x_reg = location_mat[0];
+        float y_reg = location_mat[1];
+        float w_reg = location_mat[2];
+        float h_reg = location_mat[3];
+
+        FaceInfo face_info;
+        face_info.score_ = score;
+        face_info.location_.setLeft(face.x()+ + x_reg * face.width());
+        face_info.location_.setTop(face.y() + y_reg * face.height());
+        face_info.location_.setWidth(face.x() + face.width() +
+                                     w_reg * face.width() - face_info.location_.x());
+
+        face_info.location_.setHeight(face.y() + face.height() +
+                                      h_reg * face.height() - face_info.location_.y());
+
+        second_bboxes->push_back(face_info);
+    }
+    return 0;
+}
+
+int Widget::ODetect(const ncnn::Mat & img_in,
+    const std::vector<FaceInfo>& second_bboxes,
+    std::vector<FaceInfo>* third_bboxes) {
+    third_bboxes->clear();
+    for (int i = 0; i < static_cast<int>(second_bboxes.size()); ++i) {
+        QRect face = second_bboxes.at(i).location_ & QRect(0, 0, img_in.w, img_in.h);
+        ncnn::Mat img_face, img_resized;
+        ncnn::copy_cut_border(img_in, img_face, face.y(), img_in.h - face.bottom(), face.x(), img_in.w - face.right());
+        ncnn::resize_bilinear(img_face, img_resized, 48, 48);
+
+        ncnn::Extractor ex = onet_->create_extractor();
+        ex.set_light_mode(true);
+        ex.set_num_threads(2);
+        ex.input("data", img_resized);
+        ncnn::Mat score_mat, location_mat, keypoints_mat;
+        ex.extract("prob1", score_mat);
+        ex.extract("conv6-2", location_mat);
+        ex.extract("conv6-3", keypoints_mat);
+        float score = score_mat[1];
+        if (score < threshold_[1]) continue;
+        float x_reg = location_mat[0];
+        float y_reg = location_mat[1];
+        float w_reg = location_mat[2];
+        float h_reg = location_mat[3];
+
+        FaceInfo face_info;
+        face_info.score_ = score;
+        face_info.location_.setLeft(face.x() + x_reg * face.width());
+        face_info.location_.setTop(face.y() + y_reg * face.height());
+        face_info.location_.setWidth(face.x() + face.width() +
+                                     w_reg * face.width() - face_info.location_.x());
+        face_info.location_.setHeight(face.y() + face.height() +
+                                      h_reg * face.height() - face_info.location_.y());
+
+
+
+        for (int num = 0; num < 5; num++) {
+            face_info.keypoints_[num] = face.x() + face.width() * keypoints_mat[num];
+            face_info.keypoints_[num + 5] = face.y() + face.height() * keypoints_mat[num + 5];
+        }
+
+        third_bboxes->push_back(face_info);
+    }
+    return 0;
+}
+
+int Widget::Refine(std::vector<FaceInfo>* bboxes, const QSize max_size) {
+    int num_boxes = static_cast<int>(bboxes->size());
+    for (int i = 0; i < num_boxes; ++i) {
+        FaceInfo face_info = bboxes->at(i);
+        int width = face_info.location_.width();
+        int height = face_info.location_.height();
+        float max_side = MAX(width, height);
+        face_info.location_.setLeft(face_info.location_.x() + 0.5 * width - 0.5 * max_side);
+        face_info.location_.setTop(face_info.location_.y() + 0.5 * height - 0.5 * max_side);
+        face_info.location_.setWidth(max_side);
+        face_info.location_.setHeight(max_side);
+
+
+        face_info.location_ = face_info.location_ & QRect(0, 0, max_size.width(), max_size.height());
+        bboxes->at(i) = face_info;
+    }
+
+    return 0;
+}
+
+
--- widget.h
+++ widget.h
@@ -3,7 +3,7 @@
 
 #include <QWidget>
 #include <thread>
-
+#include "ncnn/net.h"
 extern "C"
 {
 #include<stdio.h>
@@ -15,6 +15,12 @@ extern "C"
 #include<libavutil/imgutils.h>
 #include "libavutil/opt.h"
 }
+
+struct FaceInfo {
+    QRect location_;
+    float score_;
+    float keypoints_[10];
+};
 
 class Widget : public QWidget
 {
@@ -34,6 +40,17 @@ protected:
     void paintEvent(QPaintEvent*);
 
 private:
+    ncnn::Net* pnet_ = nullptr;
+    ncnn::Net* rnet_ = nullptr;
+    ncnn::Net* onet_ = nullptr;
+
+
+
+
+
+
+
+
     //变量定义*********************************************************************
     AVFormatContext *pFormatCtx;
     int i, videoStream;
@@ -57,7 +74,14 @@ private:
 
 
 
-
+    int pnet_size_;
+    int min_face_size_;
+    float scale_factor_;
+    bool initialized_;
+    const float meanVals[3] = { 127.5f, 127.5f, 127.5f };
+    const float normVals[3] = { 0.0078125f, 0.0078125f, 0.0078125f };
+    const float nms_threshold_[3] = { 0.5f, 0.7f, 0.7f };
+    const float threshold_[3] = { 0.8f, 0.8f, 0.6f };
 
 
 
@@ -70,6 +94,19 @@ private:
         AVFormatContext *mFmtCtx;
         AVStream *mOutStm;
         AVFrame *mYUVFrm;
+
+      QImage img;
+
+
+private:
+    int PDetect(const ncnn::Mat& img_in, std::vector<FaceInfo>* first_bboxes);
+    int RDetect(const ncnn::Mat& img_in, const std::vector<FaceInfo>& first_bboxes,
+        std::vector<FaceInfo>* second_bboxes);
+    int ODetect(const ncnn::Mat& img_in,
+        const std::vector<FaceInfo>& second_bboxes,
+        std::vector<FaceInfo>* third_bboxes);
+
+    int Refine(std::vector<FaceInfo>* bboxes, const QSize max_size);
 };
 
 #endif // WIDGET_H
